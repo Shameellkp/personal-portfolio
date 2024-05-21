27e6e7aa9952550c0f493dd2e7f19129c3144c75
Shameellkp <shameelrahmankp786@gmail.com>
2024-05-21 16:55:18 +0530

    personal

---
+++ script.js
@@ -0,0 +1,20 @@
+// const form = document.querySelector('form');
+
+// function sendEmail() {
+//     Email.send({
+//         Host : "smtp.elasticemail.com",
+//         Username : "shameelrahman7338@gmail.com",
+//         Password : "A73C43855FAC5CEFFF6E8C6F66398F7171A3",
+//         To : 'shameelrahman7338@gmail.com',
+//         From : "you@isp.com",
+//         Subject : "This is the subject",
+//         Body : "And this is the body"
+//     }).then(
+//       message => alert(message)
+//     );
+// }
+// form.addEventListener("submit", (e) => {
+//     e.preventDefault();
+
+//     sendEmail(); 
+// });
